35fdbbc7297ba85ef639468b2020ba2dc766a5a3
Alex Smart <aasmart@umich.edu>
2023-12-04 19:15:44 -0500

    Add another BST test

--- BinarySearchTree_tests.cpp
+++ BinarySearchTree_tests.cpp
@@ -220,6 +220,23 @@ TEST(min_greater_than_basic) {
     ASSERT_EQUAL(*tree.min_greater_than(14), 15);
 }
 
+TEST(min_greater_than_contained) {
+    BinarySearchTree<int> tree;
+    tree.insert(10);
+    tree.insert(3);
+    tree.insert(6);
+    tree.insert(15);
+    tree.insert(12);
+    tree.insert(16);
+
+    ASSERT_EQUAL(*tree.min_greater_than(3), 6);
+
+    tree.insert(5);
+
+    ASSERT_EQUAL(*tree.min_greater_than(3), 5);
+    ASSERT_EQUAL(*tree.min_greater_than(12), 15);
+}
+
 TEST(min_greater_than_none) {
     BinarySearchTree<int> tree;
     tree.insert(10);
